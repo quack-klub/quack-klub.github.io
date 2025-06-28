3e1472339791496776b43519edf8975d4d4a41f5
QuacK Klub <admin@quack.lgbt>
2025-06-28 14:56:45 +0200

    feat: update and improve
    It has now become an overengineered and awesome lil python script OwO

--- collect_symbols.py
+++ collect_symbols.py
@@ -1,40 +1,43 @@
+#! /usr/bin/env python3
+
+from ast import TypeAlias
 import os
+import sys
 from pathlib import Path
 import re
-from typing_extensions import Any
+from typing import NewType
 from bs4 import BeautifulSoup
 from requests import get, Response
 
-FILE_BASE_PATH: str = "./templates"
-FONT_OUTPUT_PATH: str = "./static/fonts/MaterialSymbols/Symbols-Outlined.woff2"
-SYMBOL_EXTRAS: list[str] = ["open_in_new"]
+FILE_BASE_PATH: Path = Path("./templates")
+FONT_OUTPUT_PATH: Path = Path("./static/fonts/MaterialSymbols/Symbols-Outlined.woff2")
+SYMBOL_EXTRAS: set[str] = {"open_in_new"}
+CSS_REGEX: re.Pattern[str] = re.compile(r"""src: url\((.+)\) format\(['"](.+)['"]\)""")
+
+CollectException = NewType("CollectException", str)
 
 
-def get_files(base: str | Path = FILE_BASE_PATH) -> list[Path]:
-    paths: list[Path] = []
-
-    for cur, sub, fs in os.walk(base):
-        for f in fs:
-            if f.endswith(".html"):
-                paths.append(Path(cur, f))
-
-    return paths
+def get_files(*, base: Path = FILE_BASE_PATH) -> list[Path]:
+    return [
+        Path(cur, f) for cur, _, fs in os.walk(base) for f in fs if f.endswith(".html")
+    ]
 
 
-def find_symbols(paths: list[Path]) -> set[str]:
+def find_symbols(paths: list[Path], /) -> set[str]:
     symbols: set[str] = set()
 
     for p in paths:
         with open(p, "r") as f:
             soup = BeautifulSoup(f.read(), features="html.parser")
 
-        for res in soup.find_all(class_="symbol"):
-            symbols.add(res.text)
+        symbols.update(map(lambda r: r.text, soup.find_all(class_="symbol")))
 
     return symbols
 
 
-def fetch_font(symbols: set[str], out: str | Path = FONT_OUTPUT_PATH) -> bool:
+def fetch_font(
+    symbols: set[str], /, *, out: Path = FONT_OUTPUT_PATH
+) -> None | CollectException:
     symbols_sorted = sorted(list(symbols))
 
     fetch_url: str = f"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names={','.join(symbols_sorted)}&display=block"
@@ -46,30 +49,31 @@ def fetch_font(symbols: set[str], out: str | Path = FONT_OUTPUT_PATH) -> bool:
         },
     )
 
-    match: re.Match[str] | None = re.search(
-        r"""src: url\((.+)\) format\(['"](.*)['"]\)""", response.text
-    )
+    match: re.Match[str] | None = re.search(CSS_REGEX, response.text)
     if match == None:
-        return False
+        return CollectException("No match found in the CSS")
 
     url: str = match.group(1)
-    format: str = match.group(2)
 
-    if format != "woff2":
-        raise Exception("Font is not woff2")
+    if (format := match.group(2)) != out.suffix[1::]:
+        return CollectException(f"Font is format {format}, expected {out.suffix[1::]}")
 
     font_file: Response = get(url)
     with open(out, "wb") as f:
         f.write(font_file.content)
 
-    return True
+    return None
 
 
 def main() -> None:
     symbols: set[str] = find_symbols(get_files())
-    symbols.update(SYMBOL_EXTRAS)
+    symbols |= SYMBOL_EXTRAS
 
-    fetch_font(symbols)
+    if err := fetch_font(symbols):
+        sys.stderr.write(err)
+        sys.stderr.write(os.linesep)
+        sys.stderr.flush()
+        sys.exit(1)
 
 
 if __name__ == "__main__":
